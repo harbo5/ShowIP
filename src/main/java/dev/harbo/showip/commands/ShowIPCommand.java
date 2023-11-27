e1198115158fad383456cd284ddd5ef03dd89e9d
harbo5 <708216+harbo5@users.noreply.github.com>
2023-11-26 19:08:28 -0500

    Removed Kyori API because it's not in older versions, updated POM to build against JDK 11 for backwards compatibility

--- src/main/java/dev/harbo/showip/commands/ShowIPCommand.java
+++ src/main/java/dev/harbo/showip/commands/ShowIPCommand.java
@@ -1,15 +1,12 @@
 package dev.harbo.showip.commands;
 
 import dev.harbo.showip.ShowIP;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.TextColor;
-import net.kyori.adventure.title.TitlePart;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
 
 public class ShowIPCommand implements CommandExecutor {
 
@@ -21,18 +18,18 @@ public class ShowIPCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (args.length != 1) {
-            sender.sendMessage(
-                    Component.text("Usage: /showip <username> - Shows the user their IP in an on screen message"));
+            sender.sendMessage("Usage: /showip <username> - Shows the user their IP in an on screen message");
         } else {
             try {
                 Player p = Bukkit.getPlayer(args[0]);
                 assert p != null;
-                p.sendTitlePart(TitlePart.TITLE, Component.text(p.getName() + ": " +
-                        p.getAddress().toString().split(":")[0].replaceAll("/",""), TextColor.color(0xFFFF55)));
+                p.sendTitle(ChatColor.YELLOW + p.getName() + ": " +
+                        p.getAddress().toString().split(":")[0].replaceAll("/",""),null, 20, 200, 20);
+
             } catch(Exception ex) {
-                sender.sendMessage(Component.text("Player not found."));
+                sender.sendMessage(ChatColor.DARK_RED + "[ShowIP]: " + ChatColor.RED + "Player not found.");
             }
         }
         return true;
